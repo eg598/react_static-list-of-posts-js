34bc42df6b684b44666b7ae5ee1259f6553cfbb3
Yegor <egor598000@gmail.com>
2026-01-08 03:13:42 +0100

    fix 2

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -10,7 +10,7 @@ export const PostInfo = ({ post }) => (
       <p>
         {' Posted by  '}
 
-        <UserInfo user={post.user} key={post.user.id} />
+        <UserInfo user={post.user} />
       </p>
     </div>
 
